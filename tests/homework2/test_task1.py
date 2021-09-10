49421ce45e7950b1d8b3b344592d4ef6e9c065fd
morin-berk <85830479+morin-berk@users.noreply.github.com>
2021-09-10 18:05:48 +0300

    Update test_task1.py

--- tests/homework2/test_task1.py
+++ tests/homework2/test_task1.py
@@ -4,20 +4,20 @@ from homework2.task1 import (count_non_ascii_chars, count_punctuation_chars,
 
 
 def test_get_longest_diverse_words():
-    assert len(get_longest_diverse_words('data.txt')) == 10
+    assert len(get_longest_diverse_words('${{ github.workspace }}/hw1/tests/homework2/data.txt')) == 10
 
 
 def test_count_punctuation_chars():
-    assert count_punctuation_chars('data.txt') > 0
+    assert count_punctuation_chars('${{ github.workspace }}/hw1/tests/homework2/data.txt') > 0
 
 
 def test_count_non_ascii_chars():
-    assert count_non_ascii_chars('data.txt') > 0
+    assert count_non_ascii_chars('${{ github.workspace }}/hw1/tests/homework2/data.txt') > 0
 
 
 def test_get_rarest_char():
-    assert get_rarest_char('data.txt') is not None
+    assert get_rarest_char('${{ github.workspace }}/hw1/tests/homework2/data.txt') is not None
 
 
 def test_get_most_common_non_ascii_char():
-    assert get_most_common_non_ascii_char('data.txt') is not ascii
+    assert get_most_common_non_ascii_char('${{ github.workspace }}/hw1/tests/homework2/data.txt') is not ascii
