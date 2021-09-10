ebb9d4bcec2743d5ed1327b988f06a1c64f11c01
morin-berk <85830479+morin-berk@users.noreply.github.com>
2021-09-10 18:08:06 +0300

    Update test_task1.py

--- tests/homework2/test_task1.py
+++ tests/homework2/test_task1.py
@@ -3,21 +3,24 @@ from homework2.task1 import (count_non_ascii_chars, count_punctuation_chars,
                              get_most_common_non_ascii_char, get_rarest_char)
 
 
+path = '${{ github.workspace }}/hw1/tests/homework2/data.txt'
+
+
 def test_get_longest_diverse_words():
-    assert len(get_longest_diverse_words('${{ github.workspace }}/hw1/tests/homework2/data.txt')) == 10
+    assert len(get_longest_diverse_words(path)) == 10
 
 
 def test_count_punctuation_chars():
-    assert count_punctuation_chars('${{ github.workspace }}/hw1/tests/homework2/data.txt') > 0
+    assert count_punctuation_chars(path) > 0
 
 
 def test_count_non_ascii_chars():
-    assert count_non_ascii_chars('${{ github.workspace }}/hw1/tests/homework2/data.txt') > 0
+    assert count_non_ascii_chars(path) > 0
 
 
 def test_get_rarest_char():
-    assert get_rarest_char('${{ github.workspace }}/hw1/tests/homework2/data.txt') is not None
+    assert get_rarest_char(path) is not None
 
 
 def test_get_most_common_non_ascii_char():
-    assert get_most_common_non_ascii_char('${{ github.workspace }}/hw1/tests/homework2/data.txt') is not ascii
+    assert get_most_common_non_ascii_char(path) is not ascii
