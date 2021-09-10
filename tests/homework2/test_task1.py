62e5bf3caf642a5e79cb335ccb44635dee033989
morin-berk <85830479+morin-berk@users.noreply.github.com>
2021-09-10 18:10:16 +0300

    Update test_task1.py

--- tests/homework2/test_task1.py
+++ tests/homework2/test_task1.py
@@ -2,7 +2,6 @@ from homework2.task1 import (count_non_ascii_chars, count_punctuation_chars,
                              get_longest_diverse_words,
                              get_most_common_non_ascii_char, get_rarest_char)
 
-
 path = '${{ github.workspace }}/hw1/tests/homework2/data.txt'
 
 
