793fa88f7343d3811585105aef8b970bc5d25648
morin-berk <85830479+morin-berk@users.noreply.github.com>
2021-09-12 20:01:52 +0300

    Update test_task1.py

--- tests/homework2/test_task1.py
+++ tests/homework2/test_task1.py
@@ -3,21 +3,24 @@ from homework2.task1 import (count_non_ascii_chars, count_punctuation_chars,
                              get_most_common_non_ascii_char, get_rarest_char)
 
 
+file = os.path.join(os.path.dirname(__file__), 'data.txt')
+
+
 def test_get_longest_diverse_words():
-    assert len(get_longest_diverse_words('data.txt')) == 10
+    assert len(get_longest_diverse_words(file)) == 10
 
 
 def test_count_punctuation_chars():
-    assert count_punctuation_chars('data.txt') > 0
+    assert count_punctuation_chars(file) > 0
 
 
 def test_count_non_ascii_chars():
-    assert count_non_ascii_chars('data.txt') > 0
+    assert count_non_ascii_chars(file) > 0
 
 
 def test_get_rarest_char():
-    assert get_rarest_char('data.txt') is not None
+    assert get_rarest_char(file) is not None
 
 
 def test_get_most_common_non_ascii_char():
-    assert get_most_common_non_ascii_char('data.txt') is not ascii
+    assert get_most_common_non_ascii_char(file) is not ascii
