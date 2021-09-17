c4ae3254018737ad096f0a8aba2550e4aa6ac499
Kseniya Bekisheva <morinberk0@gmail.com>
2021-09-17 22:56:13 +0300

    homework2 second version

--- homework2/task5.py
+++ homework2/task5.py
@@ -10,6 +10,8 @@ def custom_range(iterable: Iterable, *slices: (str, int)) -> List:
     elif len(slices) == 2:
         return [el for el in iterable if slices[0] <= el < slices[1]]
     elif len(slices) == 3 and slices[2] < 0:
-        return [el for el in iterable if slices[1] < el <= slices[0]][::slices[2]]
+        return [el for el in iterable
+                if slices[1] < el <= slices[0]][::slices[2]]
     elif len(slices) == 3 and slices[2] > 0:
-        return [el for el in iterable if slices[0] <= el < slices[1]][::slices[2]]
+        return [el for el in iterable
+                if slices[0] <= el < slices[1]][::slices[2]]
