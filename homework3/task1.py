45825b0b871b12aaeb5c11f12754e01b9d6dd5ff
Kseniya Bekisheva <morinberk0@gmail.com>
2021-10-02 14:57:35 +0300

    hw3: fix task1, improve test_task1

--- homework3/task1.py
+++ homework3/task1.py
@@ -4,26 +4,28 @@ from typing import Callable
 
 
 def cache(times):
-    """
-    Cache accepts another function as an argument. Then it
+    """Cache accepts another function as an argument. Then it
     returns such a function, so the every call to initial one
-    should be cached
-    """
+    should be cached"""
     values = {}
+    if times <= 0:
+        raise ValueError('Times` value must be greater than 0')
 
     def wrapper(func: Callable):
-
         @wraps(func)
         def memoized_func(*args, **kwargs):
-            result = func(*args, **kwargs)
             key = pickle.dumps((args, kwargs))
+            if times == 1:
+                return func(*args, **kwargs)
             if key not in values:
                 values[key] = func(*args, **kwargs)
                 values['attempt'] = times - 1
                 return values[key]
             if values['attempt'] > 0:
                 values['attempt'] -= 1
+                if values['attempt'] == 0:
+                    return values.pop(key)
                 return values[key]
-            return result
+            return func(*args, **kwargs)
         return memoized_func
     return wrapper
--- tests/homework3/test_task1.py
+++ tests/homework3/test_task1.py
@@ -1,18 +1,43 @@
+from typing import List
+
 from homework3.task1 import cache
 
 
-def test_num_cases():
-    """Testing whether caching works for func with num arguments"""
+def test_func_with_int_args():
+    """Testing whether caching works
+    for a func with int arguments"""
 
     @cache(times=2)
-    def func(a: int, b: int) -> int:
+    def func_int(a: int, b: int) -> int:
         """Just a sample of a func"""
         return (a ** b) ** 2
 
     some = 100, 200
-    val_1 = func(*some)
-    val_2 = func(*some)
-    val_3 = func(*some)
+    val_1 = func_int(*some)
+    val_2 = func_int(*some)
+    val_3 = func_int(*some)
+    val_4 = func_int(*some)
 
     assert val_1 is val_2
     assert val_1 is not val_3
+    assert val_3 is val_4
+
+
+def test_func_with_list_arg():
+    """Testing whether caching works
+    for a func with a list argument"""
+
+    @cache(times=2)
+    def func_list(a: List) -> List:
+        """Just a sample of a func"""
+        return sum(a)
+
+    some = [100, 200]
+    val_1 = func_list(some)
+    val_2 = func_list(some)
+    val_3 = func_list(some)
+    val_4 = func_list(some)
+
+    assert val_1 is val_2
+    assert val_1 is not val_3
+    assert val_3 is val_4
