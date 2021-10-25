9fed4254bd78910018ef17f0fe9becdf3d8f9cbd
Kseniya Bekisheva <morinberk0@gmail.com>
2021-10-25 11:51:44 +0300

    hw12: pylint fixed

--- homework12/mysite/polls/migrations/0001_initial.py
+++ homework12/mysite/polls/migrations/0001_initial.py
@@ -16,7 +16,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Homework',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True, primary_key=True,
+                                           serialize=False, verbose_name='ID')),
                 ('created', models.DateTimeField(default=django.utils.timezone.now)),
                 ('deadline', models.IntegerField()),
             ],
@@ -24,7 +25,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Teacher',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True, primary_key=True,
+                                           serialize=False, verbose_name='ID')),
                 ('first_name', models.CharField(max_length=50)),
                 ('second_name', models.CharField(max_length=50)),
                 ('task', models.CharField(max_length=255)),
@@ -33,26 +35,35 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Student',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True, primary_key=True,
+                                           serialize=False, verbose_name='ID')),
                 ('first_name', models.CharField(max_length=50)),
                 ('second_name', models.CharField(max_length=50)),
                 ('solution', models.TextField()),
-                ('homework', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='polls.homework')),
+                ('homework', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                                               to='polls.homework')),
             ],
         ),
         migrations.CreateModel(
             name='HomeworkResults',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True, primary_key=True,
+                                           serialize=False, verbose_name='ID')),
                 ('created', models.DateTimeField(default=django.utils.timezone.now)),
-                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='student_author', to='polls.student')),
-                ('homework', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='student_homework', to='polls.homework')),
-                ('solution', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='student_solution', to='polls.student')),
+                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                                             related_name='student_author', to='polls.student')),
+                ('homework', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                                               related_name='student_homework',
+                                               to='polls.homework')),
+                ('solution', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                                               related_name='student_solution',
+                                               to='polls.student')),
             ],
         ),
         migrations.AddField(
             model_name='homework',
             name='task',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='polls.teacher'),
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                                    to='polls.teacher'),
         ),
     ]
